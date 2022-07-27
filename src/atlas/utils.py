fccb2106b900e46258c67072341bd6c64955ab86
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-07-27 15:16:03 -0700

    start utils file

---
+++ src/atlas/utils.py
@@ -0,0 +1,15 @@
+import os
+import errno
+
+
+def mkdir_p(path):
+    """Emulates bash `mkdir -p`, in python style
+    Used for igrams directory creation
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
