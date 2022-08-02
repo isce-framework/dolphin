2136ea64cae16b6292df1dcfdf81d8fa60af0d89
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-08-02 14:33:11 -0700

    start adding yamale validators and tests

--- src/atlas/utils.py
+++ src/atlas/utils.py
@@ -1,5 +1,6 @@
 import errno
 import os
+from pathlib import Path
 
 
 def mkdir_p(path):
@@ -11,3 +12,52 @@ def mkdir_p(path):
             pass
         else:
             raise
+
+
+def get_yaml_file(name="s1_disp.yaml", yaml_type="schemas"):
+    """Get the path to a yaml schema or default file.
+
+    Parameters
+    ----------
+    name : str
+        Name of the schema
+    yaml_type : str, choices = ["schemas", "defaults"]
+        Which type of yaml file to get
+
+    Returns
+    -------
+    path : str
+        Path to the schema
+    """
+    if yaml_type not in ["schemas", "defaults"]:
+        raise ValueError("yaml_type must be one of ['schemas', 'defaults']")
+    return Path(__file__).parent / yaml_type / name
+
+
+def deep_update(original, supplied):
+    """Update the defaults of a dict with user-supplied dict.
+
+    Parameters
+    ----------
+    original : dict
+        Dict with default options to be updated
+    supplied: dict
+        Dict with user-defined options used to supplied original/default
+
+    Returns
+    -------
+    original: dict
+        Default dictionary updated with user-defined options
+
+    References
+    ----------
+    https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
+    """
+    for key, val in supplied.items():
+        if isinstance(val, dict):
+            original[key] = deep_update(original.get(key, {}), val)
+        else:
+            original[key] = val
+
+    # return updated original
+    return original
--- tests/test_atlas.py
+++ tests/test_atlas.py
@@ -1,2 +1,44 @@
+from pathlib import Path
+
+import pytest
+import yamale
+
+
 def test_import():
     import atlas  # noqa: F401
+
+
+@pytest.fixture
+def schema_file():
+    from atlas.utils import get_yaml_file
+
+    return get_yaml_file("s1_disp.yaml", yaml_type="schemas")
+
+
+@pytest.fixture
+def defaults_file():
+    from atlas.utils import get_yaml_file
+
+    return get_yaml_file("s1_disp.yaml", yaml_type="defaults")
+
+
+@pytest.fixture
+def data_file():
+    return Path(__file__).parent / "data/s1_disp_test.yaml"
+
+
+def test_yaml_loading(schema_file, defaults_file, data_file):
+    # print('!!'*20)
+    schema = yamale.make_schema(schema_file)
+    defaults = yamale.make_data(defaults_file)
+    # print(schema)
+    # print('!!'*20)
+    # print(d)
+    # print('!!'*20)
+
+    # The defaults should fail the schema
+    with pytest.raises(yamale.yamale_error.YamaleError):
+        yamale.validate(schema, defaults)
+
+    data = yamale.make_data(data_file)
+    yamale.validate(schema, data)
