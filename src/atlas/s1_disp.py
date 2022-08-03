e3943704e9326eeaab7094f910274c6809a7b16d
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-08-02 19:31:01 -0700

    workflow up to wrapped phase est

--- src/atlas/s1_disp.py
+++ src/atlas/s1_disp.py
@@ -61,14 +61,31 @@ def run(config_file: str):
         logger.info(f"Creating NMAP file {weight_file}")
         wrapped.run_nmap(
             input_vrt_file=cfg["input_vrt_file"],
+            mask_file=cfg["mask_file"],
             weight_file=str(weight_file),
             nmap_count_file=str(nmap_count_file),
             window=cfg["window"],
-            pvalue=cfg["nmap"]["pvalue"],
-            stat_method=cfg["nmap"]["stat_method"],
-            no_gpu=cfg["nmap"]["no_gpu"],
+            nmap_opts=cfg["nmap"],
             processing_opts=cfg["processing"],
-            mask_file=cfg["mask_file"],
+        )
+
+    # EVD step
+    wrapped_path = Path(cfg["directories"]["wrapped"]).absolute()
+    # For some reason fringe skips this one if the directory exists...
+    # wrapped_path.mkdir(parents=True, exist_ok=True)
+    compressed_slc_file = wrapped_path / cfg["evd"]["compressed_slc_file"]
+    if compressed_slc_file.exists():
+        logger.info(f"Skipping making existing EVD file {compressed_slc_file}")
+    else:
+        logger.info(f"Making EVD file {compressed_slc_file}")
+        wrapped.run_evd(
+            input_vrt_file=cfg["input_vrt_file"],
+            weight_file=str(weight_file),
+            compressed_slc_filename=str(compressed_slc_file.name),
+            output_folder=str(wrapped_path),
+            window=cfg["window"],
+            evd_opts=cfg["evd"],
+            processing_opts=cfg["processing"],
         )
 
 
--- src/atlas/wrapped.py
+++ src/atlas/wrapped.py
@@ -10,9 +10,7 @@ def run_nmap(
     weight_file,
     nmap_count_file,
     window,
-    pvalue,
-    stat_method,
-    no_gpu,
+    nmap_opts,
     processing_opts,
     mask_file=None,
 ):
@@ -30,41 +28,48 @@ def run_nmap(
     aa.halfWindowX = window["xhalf"]
     aa.halfWindowY = window["yhalf"]
 
-    aa.minimumProbability = pvalue
-    aa.method = stat_method
-    aa.noGPU = no_gpu
+    aa.minimumProbability = nmap_opts["pvalue"]
+    aa.method = nmap_opts["stat_method"]
 
     aa.blocksize = processing_opts["lines_per_block"]
     aa.memsize = processing_opts["ram"]
+    aa.noGPU = processing_opts["no_gpu"]
 
     aa.run()
 
 
-# time evd.py -i stack/slcs_base.vrt -o EVD -x 11 -y 5 -w nmap/nmap
-
-
-def run_evd(args):
+def run_evd(
+    *,
+    input_vrt_file,
+    weight_file,
+    compressed_slc_filename,
+    output_folder,
+    window,
+    evd_opts,
+    processing_opts,
+):
     """Run the EVD algorithm on a stack of SLCs using FRInGE."""
     import evdlib
 
+    # TODO: Check into how the multiple EVD threads are spawned
     os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
     aa = evdlib.Evd()
 
     # Explicit wiring. Can be automated later.
-    aa.inputDS = args.input_ds
-    aa.weightsDS = args.weights_ds
+    aa.inputDS = input_vrt_file
+    aa.weightsDS = weight_file
 
-    aa.outputFolder = aa.outputCompressedSlcFolder = args.output_folder
-    aa.compSlc = args.compressed_slc_file  # "compslc.bin"
+    aa.outputFolder = aa.outputCompressedSlcFolder = output_folder
+    aa.compSlc = compressed_slc_filename  # "compslc.bin"
 
-    aa.blocksize = args.linesperblock
-    aa.memsize = args.memorySize
-    aa.halfWindowX = args.halfWindowX
-    aa.halfWindowY = args.halfWindowY
-    aa.minimumNeighbors = args.min_neighbors
+    aa.minimumNeighbors = evd_opts["minimum_neighbors"]
+    aa.method = evd_opts["method"]
 
-    # Set up method and bandwidth
-    aa.method = args.method
+    aa.halfWindowX = window["xhalf"]
+    aa.halfWindowY = window["yhalf"]
+
+    aa.blocksize = processing_opts["lines_per_block"]
+    aa.memsize = processing_opts["ram"]
 
     aa.run()
