a7b2f8c87d8c81e6f14e5ad465fe586db802386a
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-08-03 10:05:02 -0700

    start making the combine ps/ds step

---
+++ src/atlas/combine_ps_ds.py
@@ -0,0 +1,13 @@
+"""Combine estimated DS phases with PS phases to form interferograms."""
+# from pathlib import Path
+# import os
+
+
+def run_combine(
+    *output_folder,
+    ps_file,
+    pl_directory,
+    temp_coh_file,
+):
+    """Run workflow step to combine PS and DS phases."""
+    pass
