f0e8e5c5d2a729f62bdd843a0d25ae8b1b6acdd8
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-10-11 09:00:58 -0700

    comments and testing

--- src/dolphin/sequential.py
+++ src/dolphin/sequential.py
@@ -19,12 +19,11 @@ def run_evd_sequential(
     *,
     slc_vrt_file: Pathlike,
     # weight_file: Pathlike,
-    # compressed_slc_file: Pathlike,
     output_folder: Pathlike,
     window: dict,
     ministack_size: int = 10,
     mask_file: Pathlike = None,
-    # lines_per_block: int = 128,
+    # ps_mask_file: Pathlike = None,
     beta: float = 0.1,
 ):
     """Estimate wrapped phase using batches of ministacks."""
@@ -43,8 +42,12 @@ def run_evd_sequential(
         xsize, ysize = get_raster_xysize(v_all.file_list[0])
         mask = np.zeros((ysize, xsize), dtype=bool)
 
-    for mini_idx, all_idx in enumerate(range(0, len(file_list_all), ministack_size)):
-        cur_files = file_list_all[all_idx : all_idx + ministack_size]
+    # Solve each ministack using the current chunk (and the previous compressed SLCs)
+    ministack_starts = range(0, len(file_list_all), ministack_size)
+    for mini_idx, full_stack_idx in enumerate(ministack_starts):
+        cur_files = file_list_all[
+            full_stack_idx : full_stack_idx + ministack_size
+        ].copy()
         name_start, name_end = cur_files[0].stem, cur_files[-1].stem
         start_end = f"{name_start}_{name_end}"
 
@@ -64,17 +67,21 @@ def run_evd_sequential(
         )
         cur_data = cur_vrt.read()
 
+        # Run the phase linking process on the current ministack
         cur_mle_stack = run_mle_gpu(
             cur_data,
             half_window=(window["xhalf"], window["yhalf"]),
             beta=beta,
+            reference_idx=mini_idx,
             mask=mask,
         )
         logger.info(f"Finished ministack {mini_idx} of size {cur_mle_stack.shape}.")
+
         # Save each of the MLE estimates (ignoring the compressed SLCs)
         for filename, cur_image in zip(cur_files[mini_idx:], cur_mle_stack[mini_idx:]):
             slc_name = filename.stem
             cur_filename = cur_output_folder / f"{slc_name}.bin"
+            # Save the MLE estimate using the slc_vrt_file's geotransform
             save_arr_like(
                 arr=cur_image,
                 like_filename=slc_vrt_file,
@@ -83,7 +90,8 @@ def run_evd_sequential(
             )
             output_slc_files[mini_idx].append(cur_filename)
 
-        cur_comp_slc = compress(cur_data, cur_mle_stack)
+        # Compress the ministack using only the non-compressed SLCs
+        cur_comp_slc = compress(cur_data[mini_idx:], cur_mle_stack[mini_idx:])
         # Save the compressed SLC
         cur_comp_slc_file = cur_output_folder / f"compressed_{start_end}.bin"
         logger.info(f"Saving compressed SLC to {cur_comp_slc_file}")
@@ -98,18 +106,19 @@ def run_evd_sequential(
         # prepend it to the VRTStack.file_list
         comp_slc_files.append(cur_comp_slc_file)
 
-    final_vrt_stack = VRTStack(
+    # Find the offsets between stacks by doing a phase linking only compressed SLCs
+    adjustment_vrt_stack = VRTStack(
         comp_slc_files, outfile=output_folder / "compressed_stack.vrt"
     )
-    logger.info(f"Running EVD on compressed files: {final_vrt_stack}")
-    comp_data = final_vrt_stack.read()
+    logger.info(f"Running EVD on compressed files: {adjustment_vrt_stack}")
+    comp_data = adjustment_vrt_stack.read()
     comp_mle_result = run_mle_gpu(
         comp_data,
         half_window=(window["xhalf"], window["yhalf"]),
     )
     comp_output_folder = output_folder / "adjustments"
     comp_output_folder.mkdir(parents=True, exist_ok=True)
-    for fname, cur_image in zip(final_vrt_stack.file_list, comp_mle_result):
+    for fname, cur_image in zip(adjustment_vrt_stack.file_list, comp_mle_result):
         name = Path(fname).stem
         cur_filename = comp_output_folder / f"{name}.bin"
         save_arr_like(
