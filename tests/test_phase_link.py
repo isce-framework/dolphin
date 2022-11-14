c5ff65d385cf2ddf337a37b413e6493c8c3049e7
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-11-14 10:32:07 -0800

    reorder to use more fixtures

--- tests/test_phase_link.py
+++ tests/test_phase_link.py
@@ -78,41 +78,46 @@ def test_full_cov(shape=(10, 100, 100), looks=(5, 5)):
     np.testing.assert_array_almost_equal(C1, C3_sub)
 
 
+# CPU versions of the MLE and EVD estimates
 @pytest.fixture
 def C_hat(slc_samples):
     return coh_mat(slc_samples)
 
 
-# @pytest.mark.skip
-def test_estimation(C_truth, C_hat):
+@pytest.fixture
+def est_mle_cpu(C_hat):
+    return np.angle(mle(C_hat))
+
+
+@pytest.fixture
+def est_evd_cpu(C_hat):
+    return np.angle(evd(C_hat))
+
+
+# Check that the estimates are close to the truth
+def test_estimation(C_truth, est_mle_cpu, est_evd_cpu):
     _, truth = C_truth
 
-    est_evd = np.angle(evd(C_hat))
-    est_mle = np.angle(mle(C_hat))
-
     err_deg = 10
-    assert np.degrees(simulate.rmse(truth, est_evd)) < err_deg
-    assert np.degrees(simulate.rmse(truth, est_mle)) < err_deg
+    assert np.degrees(simulate.rmse(truth, est_evd_cpu)) < err_deg
+    assert np.degrees(simulate.rmse(truth, est_mle_cpu)) < err_deg
 
 
 # @pytest.mark.skip
 @pytest.mark.skipif(not GPU_AVAILABLE, reason="GPU not available")
-def test_estimation_gpu(slc_samples, C_hat):
-    # Calc the CPU version
-    est_mle = np.angle(mle(C_hat))
-
+def test_estimation_gpu(slc_samples, est_mle_cpu):
     # Get the GPU version
     slc_stack = slc_samples.reshape(NUM_ACQ, 11, 11)
     est_mle_gpu_ml = mle_gpu.run_mle_multilooked_gpu(slc_stack, half_window=(5, 5))
-    assert est_mle_gpu_ml.shape == (len(est_mle), 1, 1)
+    assert est_mle_gpu_ml.shape == (len(est_mle_cpu), 1, 1)
     est_phase_gpu = np.angle(np.squeeze(est_mle_gpu_ml))
-    np.testing.assert_array_almost_equal(est_mle, est_phase_gpu, decimal=3)
+    np.testing.assert_array_almost_equal(est_mle_cpu, est_phase_gpu, decimal=3)
 
     est_mle_gpu_fullres = mle_gpu.run_mle_gpu(slc_stack, half_window=(5, 5))
-    assert est_mle_gpu_fullres.shape == (len(est_mle), 11, 11)
+    assert est_mle_gpu_fullres.shape == (len(est_mle_cpu), 11, 11)
     # The middle pixel should be the same, since it had the full window
     est_phase_gpu2 = np.angle(est_mle_gpu_fullres[:, 5, 5])
-    np.testing.assert_array_almost_equal(est_mle, est_phase_gpu2, decimal=3)
+    np.testing.assert_array_almost_equal(est_mle_cpu, est_phase_gpu2, decimal=3)
 
 
 def test_mask(slc_samples, C_truth):
