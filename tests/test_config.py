c767536093f6f8e0a42efdaa77a4f33bf5514eba
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-11-29 15:21:56 -0800

    update config

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import pytest
 import yamale
 
-import dolphin.config
-from dolphin.config import get_workflow_yaml_path
+import dolphin.workflows.config
+from dolphin.workflows.config import get_workflow_yaml_path
 
 
 # Using fixtures for these to avoid navigating the paths from tests/ to src/
@@ -42,7 +42,7 @@ def test_yaml_stack():
     defaults_file = get_workflow_yaml_path("s1_disp_stack.yaml", yaml_type="defaults")
     # Check that the updating of defaults works
     minimal_path = Path(__file__).parent / "data/s1_disp_stack_minimal.yaml"
-    min_data = dolphin.config.load_workflow_yaml(
+    min_data = dolphin.workflows.config.load_workflow_yaml(
         minimal_path, workflow_name="s1_disp_stack"
     )
     min_proc_dict = min_data["runconfig"]["groups"]["processing"]
@@ -59,22 +59,22 @@ def test_yaml_save(tmp_path):
     from ruamel.yaml import YAML
 
     minimal_path = Path(__file__).parent / "data/s1_disp_stack_minimal.yaml"
-    min_data = dolphin.config.load_workflow_yaml(
+    min_data = dolphin.workflows.config.load_workflow_yaml(
         minimal_path, workflow_name="s1_disp_stack"
     )
 
     temp_file = tmp_path / "out.yaml"
-    dolphin.config.save_yaml(temp_file, min_data)
+    dolphin.workflows.config.save_yaml(temp_file, min_data)
     y = YAML()
     assert y.load(temp_file) == min_data
 
 
 def test_dolphin_cfg_section():
     minimal_path = Path(__file__).parent / "data/s1_disp_stack_minimal.yaml"
-    min_data = dolphin.config.load_workflow_yaml(
+    min_data = dolphin.workflows.config.load_workflow_yaml(
         minimal_path, workflow_name="s1_disp_stack"
     )
-    cfg_augmented = dolphin.config.add_dolphin_section(min_data)
+    cfg_augmented = dolphin.workflows.config.add_dolphin_section(min_data)
 
     now = str(datetime.datetime.now())
     # '2022-08-03 08:47:28.834660' , ignore microseconds
