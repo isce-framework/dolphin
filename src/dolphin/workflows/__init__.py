c5a2a9329054d87d09051bddd52bf1bc63b39723
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-12-05 15:06:24 -0500

    regen docs and navigation

--- src/dolphin/workflows/__init__.py
+++ src/dolphin/workflows/__init__.py
@@ -1,8 +1,1 @@
-"""Tools for creating workflows and running displacement workflows.
-
-The workflow module provides tools for creating and running displacement
-workflows.
-
-The :class:`Config` class, provides a dictionary-like interface for accessing
-and modifying the workflow configuration.
-"""
+"""Tools for creating workflows and running displacement workflows."""
