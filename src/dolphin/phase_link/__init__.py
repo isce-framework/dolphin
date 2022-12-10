198c7e82b9632e20dffbefc91ae3a1998e4c45d6
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-12-10 10:56:05 -0500

    remove yamale stuff and fringe stuff

--- src/dolphin/phase_link/__init__.py
+++ src/dolphin/phase_link/__init__.py
@@ -3,5 +3,4 @@
 Currently implements the eigenvalue-based maximum likelihood (EMI)
 algorithm from (Ansari, 2018).
 """
-from .fringe import create_full_nmap_files, run_nmap  # noqa: F401
 from .mle import PhaseLinkRuntimeError, run_mle  # noqa: F401
