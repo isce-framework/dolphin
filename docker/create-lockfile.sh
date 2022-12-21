a18527747ca1e6c62fc485f21684cbc60c279e00
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-12-21 11:57:48 -0800

    switch docker to use OL8, add scipy as a numba dep

--- docker/create-lockfile.sh
+++ docker/create-lockfile.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -e
 
 # Check that we're running this from the repo base, since we can't add
 # a file from a parent directory
 # https://stackoverflow.com/questions/24537340/docker-adding-a-file-from-a-parent-directory
 cd "$(dirname "$0")/.."
-docker run -v "$PWD:/mnt" -w /mnt --rm -it mambaorg/micromamba:1.1.0 bash -c '\
-    micromamba install -y -c conda-forge --file conda-env.yml && micromamba env export --explicit > specfile.txt'
+docker run -u $(id -u):$(id -g) -v "$PWD:/mnt" -w /mnt --rm -it mambaorg/micromamba:1.1.0 bash -c '\
+    micromamba create --yes --name dolphin-env --file conda-env.yml && micromamba env export --name dolphin-env --explicit > /mnt/specfile.txt'
 mv specfile.txt docker/specfile.txt
