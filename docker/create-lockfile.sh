e714e0863910278f3cf6f77dcfde49be70593421
Geoffrey M Gunter <geoffrey.m.gunter@gmail.com>
2022-12-22 09:20:33 -0800

    update create-lockfile

--- docker/create-lockfile.sh
+++ docker/create-lockfile.sh
@@ -1,10 +1,46 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
 
-# Check that we're running this from the repo base, since we can't add
-# a file from a parent directory
-# https://stackoverflow.com/questions/24537340/docker-adding-a-file-from-a-parent-directory
-cd "$(dirname "$0")/.."
-docker run -u $(id -u):$(id -g) -v "$PWD:/mnt" -w /mnt --rm -it mambaorg/micromamba:1.1.0 bash -c '\
-    micromamba create --yes --name dolphin-env --file conda-env.yml && micromamba env export --name dolphin-env --explicit > /mnt/specfile.txt'
-mv specfile.txt docker/specfile.txt
+# Enable common error handling options.
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly HELP='usage: ./create-lockfile.sh ENVFILE > specfile.txt
+
+Create a conda lockfile from an environment YAML file for reproducible
+environments.
+
+positional arguments:
+ENVFILE     a YAML file containing package specifications
+
+options:
+-h, --help  show this help message and exit
+'
+
+main() {
+    # Get absolute path of input YAML file.
+    local ENVFILE
+    ENVFILE=$(realpath "$1")
+
+    # Get concretized package list.
+    local PKGLIST
+    PKGLIST=$(docker run \
+        -v "$ENVFILE:/tmp/environment.yml:ro" --rm \
+        mambaorg/micromamba:1.1.0 bash -c '\
+            micromamba install -y -n base -f /tmp/environment.yml > /dev/null && \
+            micromamba env export --explicit')
+
+    # Sort packages alphabetically.
+    # (The first 4 lines are assumed to be header lines and ignored.)
+    echo "$PKGLIST" | (sed -u 4q; sort)
+}
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+    echo "$HELP"
+elif [[ "$#" -ne 1 ]]; then
+    echo 'Illegal number of parameters' >&2
+    echo "$HELP"
+    exit 1
+else
+    main "$@"
+fi
