1f55bce9b7bd2bc22af53eaf3cbaff03e3649918
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-03-17 06:31:04 -0700

    remove the typing check without py 3.8, unpin gdal in test temporarily

--- src/dolphin/_types.py
+++ src/dolphin/_types.py
@@ -1,12 +1,6 @@
 from os import PathLike
-from pathlib import Path
-from typing import TYPE_CHECKING, Union
+from typing import Union
 
-# Some classes are declared as generic in stubs, but not at runtime.
 # In Python 3.8 and earlier, os.PathLike is not subscriptable, results in a runtime error
-if TYPE_CHECKING:
-    PathLikeStr = PathLike[str]
-else:
-    PathLikeStr = PathLike
-
-Filename = Union[str, Path, PathLikeStr]
+PathLikeStr = PathLike[str]
+Filename = Union[str, PathLikeStr]
