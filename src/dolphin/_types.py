f2ae7b137cc78df34e4c7e5d6c72a90cb1c8da63
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-03-17 06:37:04 -0700

    nope 3.9 is the min version for type subscripting

--- src/dolphin/_types.py
+++ src/dolphin/_types.py
@@ -1,6 +1,12 @@
 from os import PathLike
-from typing import Union
+from typing import TYPE_CHECKING, Union
 
-# In Python 3.8 and earlier, os.PathLike is not subscriptable, results in a runtime error
-PathLikeStr = PathLike[str]
+# Some classes are declared as generic in stubs, but not at runtime.
+# In Python 3.9 and earlier, os.PathLike is not subscriptable, results in a runtime error
+# https://stackoverflow.com/questions/71077499/typeerror-abcmeta-object-is-not-subscriptable
+if TYPE_CHECKING:
+    PathLikeStr = PathLike[str]
+else:
+    PathLikeStr = PathLike
+
 Filename = Union[str, PathLikeStr]
