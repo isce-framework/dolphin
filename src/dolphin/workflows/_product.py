aacb757dd292a0926f7be1e4c8e92e9156d6eabb
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-03-17 20:42:47 -0400

    try using h5netcdf

--- src/dolphin/workflows/_product.py
+++ src/dolphin/workflows/_product.py
@@ -29,62 +29,7 @@ GLOBAL_ATTRS = dict(
 GRID_MAPPING_DSET = "spatial_ref"
 
 
-def _create_xy_dsets(
-    group: h5py.Group, gt: List[float], shape: Tuple[int, int]
-) -> Tuple[h5py.Dataset, h5py.Dataset]:
-    """Create the x and y coordinate datasets."""
-    ysize, xsize = shape
-    # Parse the geotransform
-    x_origin, x_res, _, y_origin, _, y_res = gt
-
-    # Make the x/y arrays
-    # Note that these are the center of the pixels, whereas the GeoTransform
-    # is the upper left corner of the top left pixel.
-    x = np.arange(x_origin + x_res / 2, x_origin + x_res * xsize, x_res)
-    y = np.arange(y_origin + y_res / 2, y_origin + y_res * ysize, y_res)
-
-    # Create the datasets
-    x_ds = group.create_dataset("x_coordinates", data=x, dtype=float)
-    y_ds = group.create_dataset("y_coordinates", data=y, dtype=float)
-
-    for name, ds in zip(["x", "y"], [x_ds, y_ds]):
-        ds.make_scale(name)
-        ds.attrs["standard_name"] = f"projection_{name}_coordinate"
-        ds.attrs["long_name"] = f"{name} coordinate of projection"
-        ds.attrs["units"] = "m"
-
-    return x_ds, y_ds
-
-
-def _create_grid_mapping(group, crs: pyproj.CRS, gt: List[float]) -> h5py.Dataset:
-    """Set up the grid mapping variable."""
-    # https://github.com/corteva/rioxarray/blob/21284f67db536d9c104aa872ab0bbc261259e59e/rioxarray/rioxarray.py#L34
-    dset = group.create_dataset(GRID_MAPPING_DSET, data=np.array(0), dtype=int)
-    dset.attrs.update(crs.to_cf())
-    # Also add the GeoTransform
-    gt_string = " ".join([str(x) for x in gt])
-    dset.attrs["GeoTransform"] = gt_string
-    return dset
-
-
-def _create_correction_dsets(
-    corrections_group: h5py.Group, corrections: Dict[str, ArrayLike]
-):
-    """Create datasets for the tropospheric/ionospheric/other corrections."""
-    troposphere = corrections.get("troposphere")
-    if troposphere:
-        troposphere_dset = corrections_group.create_dataset(
-            "troposphere", data=troposphere, **DEFAULT_HDF5_OPTIONS
-        )
-        troposphere_dset.attrs["grid_mapping"] = "crs"
-
-    ionosphere = corrections["ionosphere"]
-    if ionosphere:
-        # Write the ionosphere correction image
-        ionosphere_dset = corrections_group.create_dataset(
-            "ionosphere", data=ionosphere, **DEFAULT_HDF5_OPTIONS
-        )
-        ionosphere_dset.attrs["grid_mapping"] = "crs"
+import h5netcdf
 
 
 def create_output_product(
@@ -110,12 +55,32 @@ def create_output_product(
     displacement_ds = gdal.Open(fspath(unw_filename))
     gt = displacement_ds.GetGeoTransform()
     crs = pyproj.CRS.from_wkt(displacement_ds.GetProjection())
-    arr = displacement_ds.ReadAsArray()
+    unw_arr = displacement_ds.ReadAsArray()
     displacement_ds = None
 
+    # Get the nodata mask (which for snaphu is 0)
+    mask = unw_arr == 0
+    # Set to NaN for final output
+    unw_arr[mask] = np.nan
+
+    conncomp_ds = gdal.Open(fspath(conncomp_filename))
+    conncomp_arr = conncomp_ds.ReadAsArray()
+    conncomp_ds = None
+    # the conncomp nodata is 0
+
+    fill_values = {
+        "unwrapped_phase": np.nan,
+        "connected_components": 0,
+    }
+
+    # with h5py.File(output_name, "w") as f:
     # Create the NetCDF file
-    with h5py.File(output_name, "w") as f:
+    with h5netcdf.File(output_name, "w") as f:
         f.attrs.update(GLOBAL_ATTRS)
+        f.dimensions = {
+            "y_coordinates": unw_arr.shape[0],
+            "x_coordinates": unw_arr.shape[1],
+        }
 
         # Create the '/science/SENTINEL1/DISP/grids/displacement' group
         displacement_group = f.create_group(DISP_GROUP)
@@ -124,22 +89,103 @@ def create_output_product(
         _create_grid_mapping(displacement_group, crs, gt)
 
         # Set up the X/Y variables
-        x_ds, y_ds = _create_xy_dsets(displacement_group, gt, arr.shape)
+        x_ds, y_ds = _create_xy_dsets(displacement_group, gt, unw_arr.shape)
 
-        # Write the displacement array
-        displacement_dset = displacement_group.create_dataset(
-            "displacement", data=arr, **DEFAULT_HDF5_OPTIONS
-        )
-        displacement_dset.attrs["grid_mapping"] = GRID_MAPPING_DSET
-        # Attach the X/Y coordinates
-        displacement_dset.dims[0].attach_scale(y_ds)
-        displacement_dset.dims[1].attach_scale(x_ds)
+        # Write the displacement array / conncomp arrays
+        for img, (name, fv) in zip([unw_arr, conncomp_arr], fill_values.items()):
+            # dset = displacement_group.create_dataset(
+            dset = displacement_group.create_variable(
+                name,
+                list(f.dimensions.keys()),  # h5netcdf only
+                data=img,
+                fillvalue=fv,
+                **DEFAULT_HDF5_OPTIONS,
+            )
+            dset.attrs["grid_mapping"] = GRID_MAPPING_DSET
+            # # Attach the X/Y coordinates
+            # dset.dims[0].attach_scale(y_ds)
+            # dset.dims[1].attach_scale(x_ds)
 
         # Create the '/science/SENTINEL1/DISP/corrections' group
-        corrections_group = f.require_group(CORRECTIONS_GROUP)
+        # corrections_group = f.require_group(CORRECTIONS_GROUP)
+        corrections_group = f.create_group(CORRECTIONS_GROUP)
         if corrections:
             # Write the tropospheric/ionospheric correction images (if they exist)
             _create_correction_dsets(corrections_group, corrections)
+
+
+def _create_xy(
+    gt: List[float], shape: Tuple[int, int]
+) -> Tuple[h5py.Dataset, h5py.Dataset]:
+    """Create the x and y coordinate datasets."""
+    ysize, xsize = shape
+    # Parse the geotransform
+    x_origin, x_res, _, y_origin, _, y_res = gt
+
+    # Make the x/y arrays
+    # Note that these are the center of the pixels, whereas the GeoTransform
+    # is the upper left corner of the top left pixel.
+    x = np.arange(x_origin + x_res / 2, x_origin + x_res * xsize, x_res)
+    y = np.arange(y_origin + y_res / 2, y_origin + y_res * ysize, y_res)
+    return x, y
+
+
+def _create_xy_dsets(
+    group: h5py.Group, gt: List[float], shape: Tuple[int, int]
+) -> Tuple[h5py.Dataset, h5py.Dataset]:
+    """Create the x and y coordinate datasets."""
+    x, y = _create_xy(gt, shape)
+
+    # Create the datasets
+    # x_ds = group.create_dataset("x_coordinates", data=x, dtype=float)
+    # y_ds = group.create_dataset("y_coordinates", data=y, dtype=float)
+    x_ds = group.create_variable(
+        "x_coordinates", ("x_coordinates",), np.float64, data=x
+    )
+    y_ds = group.create_variable(
+        "y_coordinates", ("y_coordinates",), np.float64, data=y
+    )
+
+    for name, ds in zip(["x", "y"], [x_ds, y_ds]):
+        # ds.make_scale(name)
+        ds.attrs["standard_name"] = f"projection_{name}_coordinate"
+        ds.attrs["long_name"] = f"{name} coordinate of projection"
+        ds.attrs["units"] = "m"
+
+    return x_ds, y_ds
+
+
+def _create_grid_mapping(group, crs: pyproj.CRS, gt: List[float]) -> h5py.Dataset:
+    """Set up the grid mapping variable."""
+    # https://github.com/corteva/rioxarray/blob/21284f67db536d9c104aa872ab0bbc261259e59e/rioxarray/rioxarray.py#L34
+    # dset = group.create_dataset(GRID_MAPPING_DSET, data=0, dtype=int)
+    dset = group.create_variable(GRID_MAPPING_DSET, data=0, dtype=int)
+
+    dset.attrs.update(crs.to_cf())
+    # Also add the GeoTransform
+    gt_string = " ".join([str(x) for x in gt])
+    dset.attrs["GeoTransform"] = gt_string
+    return dset
+
+
+def _create_correction_dsets(
+    corrections_group: h5py.Group, corrections: Dict[str, ArrayLike]
+):
+    """Create datasets for the tropospheric/ionospheric/other corrections."""
+    troposphere = corrections.get("troposphere")
+    if troposphere:
+        troposphere_dset = corrections_group.create_dataset(
+            "troposphere", data=troposphere, **DEFAULT_HDF5_OPTIONS
+        )
+        troposphere_dset.attrs["grid_mapping"] = "crs"
+
+    ionosphere = corrections["ionosphere"]
+    if ionosphere:
+        # Write the ionosphere correction image
+        ionosphere_dset = corrections_group.create_dataset(
+            "ionosphere", data=ionosphere, **DEFAULT_HDF5_OPTIONS
+        )
+        ionosphere_dset.attrs["grid_mapping"] = "crs"
 
 
 def _move_files_to_output_folder(
