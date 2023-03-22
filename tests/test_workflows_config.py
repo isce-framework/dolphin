44b9fff2f0f1c4eadcbc156d5dd3c2a2d8f04a03
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-03-21 22:55:39 -0400

    fix workflow tests

--- tests/test_workflows_config.py
+++ tests/test_workflows_config.py
@@ -76,8 +76,6 @@ def test_unwrap_options_defaults(tmpdir):
 def test_outputs_defaults(tmpdir):
     with tmpdir.as_cwd():
         opts = config.OutputOptions()
-        assert opts.output_directory == Path("output").resolve()
-        assert opts.scratch_directory == Path("scratch").resolve()
         assert opts.output_resolution is None
         assert opts.strides == {"x": 1, "y": 1}
         assert opts.hdf5_creation_options == dict(
@@ -154,20 +152,22 @@ def dir_with_2_slcs(tmp_path, slc_file_list_nc):
 
 def test_inputs_defaults(dir_with_1_slc):
     # make a dummy file
-    opts = config.Inputs(
-        cslc_file_list=dir_with_1_slc / "slclist.txt", subdataset="data"
+    opts = config.Workflow(
+        cslc_file_list=dir_with_1_slc / "slclist.txt",
+        input_options={"subdataset": "data"},
     )
 
     assert opts.cslc_file_list[0].parent == dir_with_1_slc.resolve()
-    assert opts.cslc_date_fmt == "%Y%m%d"
+    assert opts.input_options.cslc_date_fmt == "%Y%m%d"
     assert len(opts.cslc_file_list) == 1
     assert isinstance(opts.cslc_file_list[0], Path)
 
     assert opts.mask_files == []
 
     # check it's coerced to a list of Paths
-    opts2 = config.Inputs(
-        cslc_file_list=[str(opts.cslc_file_list[0])], subdataset="data"
+    opts2 = config.Workflow(
+        cslc_file_list=[str(opts.cslc_file_list[0])],
+        input_options={"subdataset": "data"},
     )
     assert isinstance(opts2.cslc_file_list[0], Path)
 
@@ -176,31 +176,36 @@ def test_inputs_bad_filename(tmp_path):
     # make a dummy file
     bad_cslc_file = tmp_path / "nonexistent_slclist.txt"
     with pytest.raises(pydantic.ValidationError, match="does not exist"):
-        config.Inputs(cslc_file_list=bad_cslc_file)
+        config.Workflow(cslc_file_list=bad_cslc_file)
 
 
 def test_input_find_slcs(slc_file_list_nc):
     cslc_dir = Path(slc_file_list_nc[0]).parent
 
-    opts = config.Inputs(cslc_file_list=slc_file_list_nc, subdataset="data")
+    opts = config.Workflow(
+        cslc_file_list=slc_file_list_nc, input_options={"subdataset": "data"}
+    )
     assert opts.cslc_file_list == slc_file_list_nc
 
-    opts2 = config.Inputs(cslc_file_list=cslc_dir / "slclist.txt", subdataset="data")
+    opts2 = config.Workflow(
+        cslc_file_list=cslc_dir / "slclist.txt", input_options={"subdataset": "data"}
+    )
     opts2.dict() == opts.dict()
 
 
 def test_input_nc_missing_subdataset(slc_file_list_nc):
     cslc_dir = Path(slc_file_list_nc[0]).parent
 
-    # opts = config.Inputs(cslc_file_list=slc_file_list_nc)
     with pytest.raises(pydantic.ValidationError, match="Must provide dataset name"):
-        config.Inputs(cslc_file_list=cslc_dir / "slclist.txt")
+        config.Workflow(cslc_file_list=cslc_dir / "slclist.txt")
 
 
 def test_input_slc_date_fmt(dir_with_2_slcs):
     expected_slcs = [Path(str(p)) for p in sorted(dir_with_2_slcs.glob("*.nc"))]
 
-    opts = config.Inputs(cslc_file_list=expected_slcs, subdataset="data")
+    opts = config.Workflow(
+        cslc_file_list=expected_slcs, input_options={"subdataset": "data"}
+    )
     assert opts.cslc_file_list == expected_slcs
 
     # If files don't all match the date format, it should error
@@ -208,7 +213,9 @@ def test_input_slc_date_fmt(dir_with_2_slcs):
     shutil.copy(expected_slcs[0], bad_date_slc)
     new_file_list = dir_with_2_slcs.glob("*.nc")
     with pytest.raises(pydantic.ValidationError):
-        opts = config.Inputs(cslc_file_list=new_file_list, subdataset="data")
+        opts = config.Workflow(
+            cslc_file_list=new_file_list, input_options={"subdataset": "data"}
+        )
 
     slc_file1 = dir_with_2_slcs / "2022-01-01.nc"
     slc_file2 = dir_with_2_slcs / "2022-01-02.nc"
@@ -216,26 +223,32 @@ def test_input_slc_date_fmt(dir_with_2_slcs):
     shutil.copy(expected_slcs[1], slc_file2)
     new_file_list = dir_with_2_slcs.glob("2022-*.nc")
 
-    opts = config.Inputs(
-        cslc_file_list=new_file_list, cslc_date_fmt="%Y-%m-%d", subdataset="data"
+    opts = config.Workflow(
+        cslc_file_list=new_file_list,
+        input_options={"cslc_date_fmt": "%Y-%m-%d", "subdataset": "data"},
     )
     assert opts.cslc_file_list == [slc_file1, slc_file2]
 
     # Check that we can get slcs by passing empty string
     # Should match all created files
     all_file_list = list(dir_with_2_slcs.glob("*.nc"))
-    opts = config.Inputs(
-        cslc_file_list=all_file_list, cslc_date_fmt="", subdataset="data"
+    opts = config.Workflow(
+        cslc_file_list=all_file_list,
+        input_options={"cslc_date_fmt": "", "subdataset": "data"},
     )
     assert set(opts.cslc_file_list) == set(all_file_list)
 
 
 def test_input_date_sort(dir_with_2_slcs):
     file_list = [Path(str(p)) for p in sorted(dir_with_2_slcs.glob("*.nc"))]
-    opts = config.Inputs(cslc_file_list=file_list, subdataset="data")
+    opts = config.Workflow(
+        cslc_file_list=file_list, input_options={"subdataset": "data"}
+    )
     assert opts.cslc_file_list == file_list
 
-    opts = config.Inputs(cslc_file_list=reversed(file_list), subdataset="data")
+    opts = config.Workflow(
+        cslc_file_list=reversed(file_list), input_options={"subdataset": "data"}
+    )
     assert opts.cslc_file_list == file_list
 
 
@@ -260,28 +273,29 @@ def test_input_opera_cslc(tmp_path, slc_stack):
         )
         file_list.append(Path(fname))
 
-    opts = config.Inputs(cslc_file_list=file_list)
+    opts = config.Workflow(cslc_file_list=file_list)
     assert opts.cslc_file_list == file_list
-    assert opts.subdataset == config.OPERA_DATASET_NAME
+    assert opts.input_options.subdataset == config.OPERA_DATASET_NAME
 
 
 def test_input_cslc_empty():
     with pytest.raises(pydantic.ValidationError):
-        config.Inputs(cslc_file_list=None)
-        config.Inputs(cslc_file_list="")
-        config.Inputs(cslc_file_list=[])
+        config.Workflow(cslc_file_list=None)
+        config.Workflow(cslc_file_list="")
+        config.Workflow(cslc_file_list=[])
 
 
 def test_config_defaults(dir_with_1_slc):
     c = config.Workflow(
-        inputs={"cslc_file_list": dir_with_1_slc / "slclist.txt", "subdataset": "data"}
+        cslc_file_list=dir_with_1_slc / "slclist.txt",
+        input_options={"subdataset": "data"},
     )
     # These should be the defaults
-    assert c.outputs == config.OutputOptions()
+    assert c.output_options == config.OutputOptions()
     assert c.worker_settings == config.WorkerSettings()
-    assert c.inputs == config.Inputs(
-        cslc_file_list=dir_with_1_slc / "slclist.txt", subdataset="data"
-    )
+    assert c.input_options == config.InputOptions(subdataset="data")
+    assert c.output_directory == Path("output").resolve()
+    assert c.scratch_directory == Path("scratch").resolve()
 
     # Check the defaults for the sub-configs, where the folders
     # should have been moved to the scratch directory
@@ -316,7 +330,9 @@ def test_config_create_dir_tree(tmpdir, slc_file_list_nc):
     shutil.copy(slc_file_list_nc[0], tmpdir / fname0)
 
     with tmpdir.as_cwd():
-        c = config.Workflow(inputs={"cslc_file_list": [fname0], "subdataset": "data"})
+        c = config.Workflow(
+            cslc_file_list=[fname0], input_options={"subdataset": "data"}
+        )
         c.create_dir_tree()
         assert c.ps_options._directory.exists()
         assert c.interferogram_network._directory.exists()
@@ -333,7 +349,8 @@ def test_config_create_dir_tree(tmpdir, slc_file_list_nc):
 
 def test_config_roundtrip_dict(dir_with_1_slc):
     c = config.Workflow(
-        inputs={"cslc_file_list": dir_with_1_slc / "slclist.txt", "subdataset": "data"}
+        cslc_file_list=dir_with_1_slc / "slclist.txt",
+        input_options={"subdataset": "data"},
     )
     c_dict = c.dict()
     c2 = config.Workflow.parse_obj(c_dict)
@@ -342,7 +359,8 @@ def test_config_roundtrip_dict(dir_with_1_slc):
 
 def test_config_roundtrip_json(dir_with_1_slc):
     c = config.Workflow(
-        inputs={"cslc_file_list": dir_with_1_slc / "slclist.txt", "subdataset": "data"}
+        cslc_file_list=dir_with_1_slc / "slclist.txt",
+        input_options={"subdataset": "data"},
     )
     c_json = c.json()
     c2 = config.Workflow.parse_raw(c_json)
@@ -352,7 +370,8 @@ def test_config_roundtrip_json(dir_with_1_slc):
 def test_config_roundtrip_yaml(tmp_path, dir_with_1_slc):
     outfile = tmp_path / "config.yaml"
     c = config.Workflow(
-        inputs={"cslc_file_list": dir_with_1_slc / "slclist.txt", "subdataset": "data"}
+        cslc_file_list=dir_with_1_slc / "slclist.txt",
+        input_options={"subdataset": "data"},
     )
     c.to_yaml(outfile)
     c2 = config.Workflow.from_yaml(outfile)
@@ -362,7 +381,8 @@ def test_config_roundtrip_yaml(tmp_path, dir_with_1_slc):
 def test_config_roundtrip_yaml_with_comments(tmp_path, dir_with_1_slc):
     outfile = tmp_path / "config.yaml"
     c = config.Workflow(
-        inputs={"cslc_file_list": dir_with_1_slc / "slclist.txt", "subdataset": "data"}
+        cslc_file_list=dir_with_1_slc / "slclist.txt",
+        input_options={"subdataset": "data"},
     )
     c.to_yaml(outfile, with_comments=True)
     c2 = config.Workflow.from_yaml(outfile)
