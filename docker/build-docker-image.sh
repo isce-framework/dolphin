2e5c188fa7aad304a28e23c3e0f983ef138d73d7
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-03-23 18:43:51 -0700

    add running commands to the build script

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -39,3 +39,17 @@ if [ -z "${MAMBA_USER_ID+x}" ]; then
 else
     docker build --network=host -t opera-adt/dolphin:"$TAG" --build-arg MAMBA_USER_ID="$MAMBA_USER_ID" --file docker/Dockerfile .
 fi
+
+# To run the image and see the help message....
+# docker run --rm -it opera-adt/dolphin:latest --help
+#
+# To run the workflow on a configuration in the current directory....
+# docker run --user $(id -u):$(id -g) -v $PWD:/app --rm -it opera-adt/dolphin:latest run dolphin_config.yaml
+# where...
+#     --user $(id -u):$(id -g)  # Needed to avoid permission issues when writing to the mounted volume.
+#     -v $PWD:/app  # Mounts the current directory to the /app directory in the container.
+#     --rm  # Removes the container after it exits.
+#     -it  # Needed to keep the container running after the command exits.
+#     opera-adt/dolphin:latest  # The name of the image to run.
+#     run  # The `dolphin` command that is run in the container (i.e. `dolphin run`).
+#     dolphin_config.yaml # The configuration file
