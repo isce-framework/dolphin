c5adc90d9013b2c6e779f575de700550005c61a2
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-04-17 15:50:41 -0700

    move in old functions about eroding data

--- src/dolphin/workflows/_utils.py
+++ src/dolphin/workflows/_utils.py
@@ -1,17 +1,21 @@
 import itertools
 import re
 import subprocess
+from os import fspath
 from pathlib import Path
 from typing import Dict, List, Optional, Pattern, Sequence, Union
 
 import h5py
-from osgeo import ogr
+import numpy as np
+from osgeo import gdal
+from shapely import wkt
+from shapely.ops import unary_union
 
 from dolphin import io
 from dolphin._log import get_log
 from dolphin._types import Filename
 
-from .config import OPERA_BURST_RE
+from .config import OPERA_BURST_RE, OPERA_DATASET_NAME
 
 logger = get_log(__name__)
 
@@ -165,29 +169,57 @@ def setup_output_folder(
     return output_files
 
 
-def get_union_polygon(opera_file_list: List[Filename]):
+def get_union_polygon(opera_file_list: List[Filename], buffer_degrees: float = 0.0):
     """Get the union of the bounding polygons of the given files.
 
     Parameters
     ----------
     opera_file_list : List[Filename]
         List of COMPASS SLC filenames.
+    buffer_degrees : float, optional
+        Buffer the polygons by this many degrees, by default 0.0
     """
     polygons = []
     dset_name = "science/SENTINEL1/identification/bounding_polygon"
     for f in opera_file_list:
         with h5py.File(f) as hf:
             wkt_str = hf[dset_name][()].decode("utf-8")
-        geom = ogr.CreateGeometryFromWkt(wkt_str)
+        # geom = ogr.CreateGeometryFromWkt(wkt_str)
+        geom = wkt.loads(wkt_str).buffer(buffer_degrees)
         polygons.append(geom)
 
-    total_poly = polygons[0]
-    for geom in polygons[1:]:
-        total_poly = total_poly.Union(geom)
-    return total_poly
+    # Union all the polygons
+    return unary_union(polygons)
+
+    # total_poly = polygons[0]
+    # for geom in polygons[1:]:
+    #     total_poly = total_poly.Union(geom)
+    # return total_poly
 
 
-def make_nodata_mask(opera_file_list: List[Filename], out_file: Filename):
+def intersection_over_union(poly1, poly2):
+    """Compute the intersection over union of two polygons.
+
+    Parameters
+    ----------
+    poly1 : shapely.geometry.Polygon
+        First polygon.
+    poly2 : shapely.geometry.Polygon
+        Second polygon.
+
+    Returns
+    -------
+    float
+        Intersection over union of the two polygons.
+    """
+    intersection = poly1.intersection(poly2)
+    union = poly1.union(poly2)
+    return intersection.area / union.area
+
+
+def make_nodata_mask(
+    opera_file_list: List[Filename], out_file: Filename, buffer_pixels: int = 0
+):
     """Make a dummy raster from the first file in the list.
 
     Parameters
@@ -196,11 +228,20 @@ def make_nodata_mask(opera_file_list: List[Filename], out_file: Filename):
         List of COMPASS SLC filenames.
     out_file : Filename
         Output filename.
+    buffer_pixels : int, optional
+        Number of pixels to buffer the union polygon by, by default 0.
+        Note that buffering will *decresase* the numba of pixels marked as nodata.
+        This is to be more conservative to not mask possible valid pixels.
     """
-    from dolphin.workflows.config import OPERA_DATASET_NAME
+    # buffer_pixels = min(buffer_pixels, min(ds.RasterXSize, ds.RasterYSize))
+    # convert pixels to degrees lat/lon
+    gt = io.get_raster_gt(opera_file_list[0])
+    dx_meters = gt[1]
+    dx_degrees = dx_meters / 111000
+    buffer_degrees = buffer_pixels * dx_degrees
 
     # Get the union of all the polygons and convert to a temp geojson
-    union_poly = get_union_polygon(opera_file_list)
+    union_poly = get_union_polygon(opera_file_list, buffer_degrees=buffer_degrees)
     temp_vector = "temp.geojson"
     with open(temp_vector, "w") as f:
         f.write(union_poly.ExportToJson())
@@ -222,3 +263,105 @@ def make_nodata_mask(opera_file_list: List[Filename], out_file: Filename):
 
     # Clean up the temp file
     Path(temp_vector).unlink()
+
+
+def get_stack_nodata_mask(
+    stack_filename: Filename,
+    output_file: Optional[Filename] = None,
+    compute_bands: Optional[List[int]] = None,
+    buffer_pixels: int = 100,
+    nodata: float = np.nan,
+):
+    """Get a mask of pixels that are nodata in all bands of `slc_stack_vrt`.
+
+    Parameters
+    ----------
+    stack_filename : Path or str
+        File containing the SLC stack as separate bands.
+    output_file : Path or str, optional
+        Name of file to save to., by default None
+    compute_bands : List[int], optional
+        List of bands in vrt_stack to read.
+        If None, reads in the first, middle, and last images.
+    buffer_pixels : int, optional
+        Number of pixels to expand the good-data area, by default 100
+    nodata : float, optional
+        Value of no data in the vrt_stack, by default np.nan
+
+    Returns
+    -------
+    mask : np.ndarray[bool]
+        Array where True indicates all bands are nodata.
+    """
+    ds = gdal.Open(fspath(stack_filename))
+    if compute_bands is None:
+        count = ds.RasterCount
+        # Get the first and last file
+        compute_bands = list(sorted(set([1, count])))
+
+    # Start with ones, then only keep pixels that are nodata
+    # in all the bands we check (reducing using logical_and)
+    out_mask = np.ones((ds.RasterYSize, ds.RasterXSize), dtype=bool)
+
+    # cap buffer pixel length to be no more the image size
+    buffer_pixels = min(buffer_pixels, min(ds.RasterXSize, ds.RasterYSize))
+    for b in compute_bands:
+        logger.debug(f"Computing mask for band {b}")
+        bnd = ds.GetRasterBand(b)
+        arr = bnd.ReadAsArray()
+        if np.isnan(nodata):
+            nodata_mask = np.isnan(arr)
+        else:
+            nodata_mask = arr == nodata
+
+        # Expand the region with a convolution
+        if buffer_pixels > 0:
+            logger.debug(f"Padding mask with {buffer_pixels} pixels")
+            out_mask &= _erode_nodata(nodata_mask, buffer_pixels)
+        else:
+            out_mask &= nodata_mask
+
+    if output_file:
+        io.write_arr(
+            arr=out_mask,
+            output_name=output_file,
+            like_filename=stack_filename,
+            nbands=1,
+            dtype="Byte",
+        )
+    return out_mask
+
+
+def _erode_nodata(nd_mask, buffer_pixels=25):
+    """Erode the nodata mask by `buffer_pixels`.
+
+    This makes the nodata mask more conservative:
+    there will be fewer pixels marked as nodata after.
+
+    Parameters
+    ----------
+    nd_mask : np.ndarray[bool]
+        Array where True indicates nodata.
+    buffer_pixels : int, optional
+        Size (in pixels) of erosion structural element to use.
+        By default 25.
+
+    Returns
+    -------
+    np.ndarray[bool]
+        Same size as `nd_mask`, with no data pixels shrunk
+        after erosion.
+    """
+    # invert so that good pixels are 1
+    # we want to expand the area that is considered "good"
+    # since we're being conservative with what we completely ignore
+    out = (~nd_mask).astype("float32").copy()
+    strel = np.ones(buffer_pixels)
+    for i in range(out.shape[0]):
+        o = np.convolve(out[i, :], strel, mode="same")
+        out[i, :] = o
+    for j in range(out.shape[1]):
+        o = np.convolve(out[:, j], strel, mode="same")
+        out[:, j] = o
+    # convert back to binary mask, and re-invert
+    return ~(out > 1e-3)
