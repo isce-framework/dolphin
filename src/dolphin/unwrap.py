3acf921b43d877f45947ffa7d4e1f3e3235a19b8
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-05-11 12:22:15 -0700

    add start of method to compute adjacent-pixel phase jumps

--- src/dolphin/unwrap.py
+++ src/dolphin/unwrap.py
@@ -9,6 +9,7 @@ from typing import Optional
 import isce3
 import numpy as np
 from isce3.unwrap import ICU, snaphu
+from numba import njit, stencil
 from osgeo import gdal
 
 from dolphin import io
@@ -307,3 +308,34 @@ def _zero_from_mask(
             like_filename=corr_filename,
         )
     return zeroed_ifg_file, zeroed_corr_file
+
+
+@njit(nogil=True)
+def compute_phase_diffs(phase):
+    """Compute the total number phase jumps > pi between adjacent pixels.
+
+    If part of `phase` is known to be bad phase (e.g. over water),
+    the values should be set to zero or a masked array should be passed:
+
+        unwrapping_error(np.ma.masked_where(bad_area_mask, phase))
+
+
+    Parameters
+    ----------
+    phase : ArrayLike
+        Unwrapped interferogram phase.
+
+    Returns
+    -------
+    int
+        Total number of jumps exceeding pi.
+    """
+    return _compute_phase_diffs(phase)
+
+
+@stencil
+def _compute_phase_diffs(phase):
+    d1 = np.abs(phase[0, 0] - phase[0, 1]) / np.pi
+    d2 = np.abs(phase[0, 0] - phase[1, 0]) / np.pi
+    # Subtract 0.5 so that anything below 1 gets rounded to 0
+    return round(d1 - 0.5) + round(d2 - 0.5)
