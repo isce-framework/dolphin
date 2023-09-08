5cd82b42cc2251b77bf1999f8f4a9b8dce61c1b7
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-09-07 18:44:44 -0700

    update show_versions for gpu optional deps

--- src/dolphin/_show_versions.py
+++ src/dolphin/_show_versions.py
@@ -31,23 +31,6 @@ def _get_sys_info() -> dict[str, str]:
     }
 
 
-def _get_opera_info() -> dict[str, Optional[str]]:
-    """Information on system on core modules.
-
-    Returns
-    -------
-    dict
-        dolphin / opera module information
-    """
-    blob = {
-        "dolphin": dolphin.__version__,
-        # optionals
-        "isce3": _get_version("isce3"),
-        "compass": _get_version("compass"),
-    }
-    return blob
-
-
 def _get_version(module_name: str) -> Optional[str]:
     if module_name in sys.modules:
         mod = sys.modules[module_name]
@@ -60,6 +43,23 @@ def _get_version(module_name: str) -> Optional[str]:
         return mod.__version__
     except AttributeError:
         return mod.version
+
+
+def _get_opera_info() -> dict[str, Optional[str]]:
+    """Information on isce/opera specific modules.
+
+    Returns
+    -------
+    dict
+        dolphin / opera module information
+    """
+    blob = {
+        "dolphin": dolphin.__version__,
+        # optionals
+        "isce3": _get_version("isce3"),
+        "tophu": _get_version("tophu"),
+    }
+    return blob
 
 
 def _get_deps_info() -> dict[str, Optional[str]]:
@@ -82,6 +82,21 @@ def _get_deps_info() -> dict[str, Optional[str]]:
     return {name: _get_version(name) for name in deps}
 
 
+def _get_gpu_info() -> dict[str, Optional[str]]:
+    """Overview of the optional GPU packages.
+
+    Returns
+    -------
+    dict:
+        version information on relevant Python libraries
+    """
+    deps = [
+        "cudatoolkit",
+        "cupy",
+    ]
+    return {name: _get_version(name) for name in deps}
+
+
 def _print_info_dict(info_dict: dict) -> None:
     """Print the information dictionary."""
     for key, stat in info_dict.items():
@@ -95,9 +110,14 @@ def show_versions() -> None:
     --------
     > python -c "import dolphin; dolphin.show_versions()"
     """
-    print("dolphin info:")
+    from dolphin.utils import gpu_is_available
+
+    print("dolphin/isce info:")
     _print_info_dict(_get_opera_info())
     print("\nSystem:")
     _print_info_dict(_get_sys_info())
     print("\nPython deps:")
     _print_info_dict(_get_deps_info())
+    print("optional GPU info:")
+    print(f"{gpu_is_available() = }")
+    _print_info_dict(_get_gpu_info())
