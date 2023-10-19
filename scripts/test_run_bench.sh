91de08539867a9ce35e3c86f0c92358193ee13e5
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2023-10-19 11:21:53 -0700

    use export for test script

--- scripts/test_run_bench.sh
+++ scripts/test_run_bench.sh
@@ -3,10 +3,12 @@ set -e
 
 # Save the commits hashes for all tagged releases
 # Only look for ones past version 0.4.1 (which added `evd` option)
-git show-ref --tags | awk '$2>="refs/tags/v0.4.1" { print $1; } ' >tag_hashlist.txt
+git show-ref --tags | awk '$2 >= "refs/tags/v0.4.2" { print $1; } ' >tag_hashlist.txt
 
 num_threads=${NUM_THREADS:-16}
-NUMBA_NUM_THREADS=$num_threads
-OMP_NUM_THREADS=$num_threads
-OPENBLAS_NUM_THREADS=$num_threads
-asv run HASHFILE:tag_hashlist.txt --cpu-affinity 0-15 --show-stderr
+export NUMBA_NUM_THREADS=$num_threads
+export OMP_NUM_THREADS=$num_threads
+export OPENBLAS_NUM_THREADS=$num_threads
+asv run HASHFILE:tag_hashlist.txt --skip-existing-commits \
+    --cpu-affinity 0-15 \
+    --show-stderr
