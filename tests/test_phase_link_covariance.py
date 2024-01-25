c587790a98b60973637cbe9616d6798a0f9591ad
scott <scott.stanie@gmail.com>
2024-01-25 02:20:25 -0500

    simplify cov tests

--- tests/test_phase_link_covariance.py
+++ tests/test_phase_link_covariance.py
@@ -1,12 +1,13 @@
 import os
-from math import ceil, floor
+from math import floor
 
 import numpy as np
 import numpy.testing as npt
 import pytest
 
+from dolphin._types import HalfWindow, Strides
 from dolphin.phase_link import covariance, simulate
-from dolphin.utils import compute_out_shape, gpu_is_available, take_looks
+from dolphin.utils import gpu_is_available, take_looks
 
 GPU_AVAILABLE = gpu_is_available() and os.environ.get("NUMBA_DISABLE_JIT") != "1"
 NUM_ACQ = 30
@@ -64,95 +65,19 @@ def test_coh_mat_single(slcs, expected_cov, looks=(5, 5)):
             r_slice = slice(r * r_looks, (r + 1) * r_looks)
             c_slice = slice(c * c_looks, (c + 1) * c_looks)
             cur_samples = slcs[:, r_slice, c_slice].reshape(num_slc, -1)
-            cur_C = covariance.coh_mat_single(
-                cur_samples, neighbor_mask=np.empty((0,), dtype=bool)
-            )
+            cur_C = covariance.coh_mat_single(cur_samples)
             npt.assert_array_almost_equal(expected_cov[r, c, :, :], cur_C)
 
 
 def test_estimate_stack_covariance_cpu(slcs, expected_cov, looks=(5, 5)):
     # Check the full stack function
     r_looks, c_looks = looks
-    half_window = {"x": c_looks // 2, "y": r_looks // 2}
-    strides = {"x": c_looks, "y": r_looks}
-    C1_cpu = covariance.estimate_stack_covariance_cpu(
+    half_window = HalfWindow(c_looks // 2, r_looks // 2)
+    strides = Strides(c_looks, r_looks)
+    C1_cpu = covariance.estimate_stack_covariance(
         slcs, half_window=half_window, strides=strides
     )
     npt.assert_array_almost_equal(expected_cov, C1_cpu)
-
-    # Check multi-processing
-    C1_cpu_mp = covariance.estimate_stack_covariance_cpu(
-        slcs, half_window=half_window, strides=strides, n_workers=2
-    )
-    npt.assert_array_almost_equal(expected_cov, C1_cpu_mp)
-
-
-@pytest.mark.skipif(not GPU_AVAILABLE, reason="GPU not available")
-def test_estimate_stack_covariance_gpu(slcs, expected_cov, looks=(5, 5)):
-    import cupy as cp
-
-    num_slc, rows, cols = slcs.shape
-    # Get the CPU version for comparison
-    expected_cov = get_expected_cov(slcs, looks)
-
-    # Set up the full res version using numba
-    d_slcs = cp.asarray(slcs)
-
-    strides = {"x": 1, "y": 1}
-    out_rows, out_cols = rows, cols
-    d_C3 = cp.zeros((out_rows, out_cols, num_slc, num_slc), dtype=np.complex64)
-    threads_per_block = (16, 16)
-    blocks_x = ceil(slcs.shape[1] / threads_per_block[0])
-    blocks_y = ceil(slcs.shape[2] / threads_per_block[1])
-    blocks = (blocks_x, blocks_y)
-
-    halfwin_rowcol = (looks[0] // 2, looks[1] // 2)
-    strides_rowcol = (strides["y"], strides["x"])
-
-    d_neighbor_arrays = cp.zeros((1, 1, 1, 1), dtype=np.bool_)
-    do_shp = False
-    covariance.estimate_stack_covariance_gpu[blocks, threads_per_block](
-        d_slcs, halfwin_rowcol, strides_rowcol, d_neighbor_arrays, d_C3, do_shp
-    )
-    C3 = d_C3.get()
-    # assert C3.shape == (out_rows, out_cols, num_slc, num_slc)
-    C3_sub = C3[2 : -2 : looks[0], 2 : -2 : looks[0]]
-    assert C3_sub.shape == expected_cov.shape
-    npt.assert_array_almost_equal(expected_cov, C3_sub)
-
-
-@pytest.mark.skipif(not GPU_AVAILABLE, reason="GPU not available")
-def test_estimate_stack_covariance_gpu_strides(slcs, expected_cov, looks=(5, 5)):
-    import cupy as cp
-
-    num_slc, rows, cols = slcs.shape
-    # Get the CPU version for comparison
-    expected_cov = get_expected_cov(slcs, looks)
-
-    # Set up the full res version using numba
-    d_slcs = cp.asarray(slcs)
-
-    r_looks, c_looks = looks
-    strides = {"x": c_looks, "y": r_looks}
-    out_rows, out_cols = compute_out_shape((rows, cols), strides)
-    d_neighbor_arrays = cp.zeros((1, 1, 1, 1), dtype=np.bool_)
-    do_shp = False
-    d_C3 = cp.zeros((out_rows, out_cols, num_slc, num_slc), dtype=np.complex64)
-
-    threads_per_block = (16, 16)
-    blocks_x = ceil(slcs.shape[1] / threads_per_block[0])
-    blocks_y = ceil(slcs.shape[2] / threads_per_block[1])
-    blocks = (blocks_x, blocks_y)
-
-    halfwin_rowcol = (looks[0] // 2, looks[1] // 2)
-    strides_rowcol = (strides["y"], strides["x"])
-    covariance.estimate_stack_covariance_gpu[blocks, threads_per_block](
-        d_slcs, halfwin_rowcol, strides_rowcol, d_neighbor_arrays, d_C3, do_shp
-    )
-    # Now this should be the same size as the multi-looked version
-    C3 = d_C3.get()
-    assert C3.shape == expected_cov.shape
-    npt.assert_array_almost_equal(expected_cov, C3)
 
 
 def test_estimate_stack_covariance_nans(slcs):
@@ -172,56 +97,17 @@ def test_estimate_stack_covariance_nans(slcs):
     slc_samples_nan = slc_stack_nan.reshape(num_slc, -1)
     # TODO: should we raise an error if we pass a dead SLC?
     # with pytest.raises(ZeroDivisionError):
-    assert covariance.coh_mat_single(slc_samples_nan)[0, 1] == 0
-    assert covariance.coh_mat_single(slc_samples_nan)[1, 0] == 0
+    # The 2nd row, and whole 2nd col are all nans
+    assert np.isnan(covariance.coh_mat_single(slc_samples_nan)[:, 1]).all()
+    assert np.isnan(covariance.coh_mat_single(slc_samples_nan)[1, :]).all()
 
     # all nans should return an identity matrix (no correlation anywhere)
     slc_stack_nan = slcs.copy()
     slc_stack_nan[:, :, :] = np.nan
     slc_samples_nan = slc_stack_nan.reshape(num_slc, -1)
-    assert covariance.coh_mat_single(slc_samples_nan).sum() == num_slc
-
-
-@pytest.mark.skipif(not GPU_AVAILABLE, reason="GPU not available")
-def test_estimate_stack_covariance_nans_gpu(slcs, looks=(5, 5)):
-    import cupy as cp
-
-    # Set up the full res version using numba
-    num_slc, rows, cols = slcs.shape
-    threads_per_block = (16, 16)
-    blocks_x = ceil(rows / threads_per_block[0])
-    blocks_y = ceil(cols / threads_per_block[1])
-    blocks = (blocks_x, blocks_y)
-    d_slcs = cp.asarray(slcs)
-    d_C = cp.zeros((rows, cols, num_slc, num_slc), dtype=np.complex64)
-    d_neighbor_arrays = cp.zeros((1, 1, 1, 1), dtype=np.bool_)
-    do_shp = False
-
-    halfwin_rowcol = (looks[0] // 2, looks[1] // 2)
-    strides_rowcol = (1, 1)
-    covariance.estimate_stack_covariance_gpu[blocks, threads_per_block](
-        d_slcs, halfwin_rowcol, strides_rowcol, d_neighbor_arrays, d_C, do_shp
-    )
-    C_nonan = d_C.get()
-
-    slcs_nan = slcs.copy()
-    slcs_nan[:, 20, 20] = np.nan
-    d_slcs_nan = cp.asarray(slcs_nan)
-    d_C_nan = cp.zeros((rows, cols, num_slc, num_slc), dtype=np.complex64)
-    covariance.estimate_stack_covariance_gpu[blocks, threads_per_block](
-        d_slcs_nan,
-        halfwin_rowcol,
-        strides_rowcol,
-        d_neighbor_arrays,
-        d_C_nan,
-        do_shp,
-    )
-    C_nan = d_C_nan.get()
-
-    # Make sure it doesn't affect pixels far away
-    assert np.abs(C_nonan[5, 5] - C_nan[5, 5]).max() < 1e-6
-    # Should still be close to the non-nan version
-    assert np.max(np.abs(C_nonan - C_nan)) < 0.10
+    # TODO: do we want an identity matrix? Or for it to be nans?
+    # assert covariance.coh_mat_single(slc_samples_nan).sum() == num_slc
+    assert np.isnan(covariance.coh_mat_single(slc_samples_nan)).all()
 
 
 def test_estimate_stack_covariance_neighbors(slcs):
