b220e9fc2e0c39a24928ad043da49048bdc8a5d0
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-02-07 21:58:23 -0500

    reorg test unwrap, make the failing test for conncomp

--- tests/test_unwrap.py
+++ tests/test_unwrap.py
@@ -24,72 +24,24 @@ pytestmark = pytest.mark.filterwarnings(
 
 
 @pytest.fixture()
-def corr_raster(raster_100_by_200):
-    # Make a correlation raster of all 1s in the same directory as the raster
-    d = Path(raster_100_by_200).parent
-    corr_raster = d / "corr_raster.bin"
-    io.write_arr(
-        arr=np.ones((100, 200), dtype=np.float32),
-        output_name=corr_raster,
-        like_filename=raster_100_by_200,
-        driver="ENVI",
-    )
-    return corr_raster
+def list_of_gtiff_ifgs(tmp_path, raster_100_by_200):
+    ifg_list = []
+    for i in range(3):
+        # Create a copy of the raster in the same directory
+        f = tmp_path / f"ifg_{i}.int.tif"
+        io.write_arr(
+            arr=np.ones((100, 200), dtype=np.complex64),
+            output_name=f,
+            like_filename=raster_100_by_200,
+            driver="GTiff",
+        )
+        ifg_list.append(f)
 
-
-def test_unwrap_snaphu(tmp_path, raster_100_by_200, corr_raster):
-    unw_filename = tmp_path / "unwrapped.unw.tif"
-    unw_path, conncomp_path = dolphin.unwrap.unwrap(
-        ifg_filename=raster_100_by_200,
-        corr_filename=corr_raster,
-        unw_filename=unw_filename,
-        nlooks=1,
-        init_method="mst",
-    )
-    assert unw_path == unw_filename
-    assert str(conncomp_path) == str(unw_filename).replace(
-        ".unw.tif", ".unw.conncomp.tif"
-    )
-    assert io.get_raster_xysize(unw_filename) == io.get_raster_xysize(raster_100_by_200)
-
-    # test other init_method
-    unw_path, conncomp_path = dolphin.unwrap.unwrap(
-        ifg_filename=raster_100_by_200,
-        corr_filename=corr_raster,
-        unw_filename=unw_filename,
-        nlooks=1,
-        init_method="mcf",
-    )
-
-
-@pytest.mark.parametrize("method", [UnwrapMethod.ICU, UnwrapMethod.PHASS])
-def test_unwrap_methods(tmp_path, raster_100_by_200, corr_raster, method):
-    unw_filename = tmp_path / f"{method.value}_unwrapped.unw.tif"
-    dolphin.unwrap.unwrap(
-        ifg_filename=raster_100_by_200,
-        corr_filename=corr_raster,
-        unw_filename=unw_filename,
-        nlooks=1,
-        unwrap_method=method,
-    )
-
-
-def test_unwrap_logfile(tmp_path, raster_100_by_200, corr_raster):
-    unw_filename = tmp_path / "unwrapped.unw.tif"
-    dolphin.unwrap.unwrap(
-        ifg_filename=raster_100_by_200,
-        corr_filename=corr_raster,
-        unw_filename=unw_filename,
-        nlooks=1,
-        unwrap_method="icu",
-        log_to_file=True,
-    )
-    logfile_name = str(unw_filename).replace(".unw.tif", ".unw.log")
-    assert Path(logfile_name).exists()
+    return ifg_list
 
 
 @pytest.fixture()
-def list_of_ifgs(tmp_path, raster_100_by_200):
+def list_of_envi_ifgs(tmp_path, raster_100_by_200):
     ifg_list = []
     for i in range(3):
         # Create a copy of the raster in the same directory
@@ -106,33 +58,108 @@ def list_of_ifgs(tmp_path, raster_100_by_200):
     return ifg_list
 
 
-def test_run(list_of_ifgs, corr_raster):
-    ifg_path = list_of_ifgs[0].parent
-    dolphin.unwrap.run(
-        ifg_filenames=list_of_ifgs,
-        cor_filenames=[corr_raster] * len(list_of_ifgs),
-        output_path=ifg_path,
-        nlooks=1,
-        init_method="mst",
-        max_jobs=1,
-    )
-
-
 @pytest.fixture()
-def list_of_gtiff_ifgs(tmp_path, raster_100_by_200):
-    ifg_list = []
-    for i in range(3):
-        # Create a copy of the raster in the same directory
-        f = tmp_path / f"ifg_{i}.int.tif"
-        io.write_arr(
-            arr=np.ones((100, 200), dtype=np.complex64),
-            output_name=f,
-            like_filename=raster_100_by_200,
-            driver="GTiff",
-        )
-        ifg_list.append(f)
+def corr_raster(raster_100_by_200):
+    # Make a correlation raster of all 1s in the same directory as the raster
+    d = Path(raster_100_by_200).parent
+    corr_raster = d / "corr_raster.cor.tif"
+    io.write_arr(
+        arr=np.ones((100, 200), dtype=np.float32),
+        output_name=corr_raster,
+        like_filename=raster_100_by_200,
+        driver="GTiff",
+    )
+    return corr_raster
 
-    return ifg_list
+
+class TestUnwrapSingle:
+    def test_unwrap_snaphu_default(self, tmp_path, list_of_gtiff_ifgs, corr_raster):
+        unw_filename = tmp_path / "unwrapped.unw.tif"
+        unw_path, conncomp_path = dolphin.unwrap.unwrap(
+            ifg_filename=list_of_gtiff_ifgs[0],
+            corr_filename=corr_raster,
+            unw_filename=unw_filename,
+            nlooks=1,
+        )
+        assert unw_path == unw_filename
+        assert str(conncomp_path) == str(unw_filename).replace(
+            ".unw.tif", ".unw.conncomp.tif"
+        )
+        assert io.get_raster_xysize(unw_filename) == io.get_raster_xysize(
+            list_of_gtiff_ifgs[0]
+        )
+
+    @pytest.mark.parametrize("init_method", ["mst", "mcf"])
+    def test_unwrap_snaphu(
+        self, tmp_path, list_of_gtiff_ifgs, corr_raster, init_method
+    ):
+        # test other init_method
+        unw_filename = tmp_path / "unwrapped.unw.tif"
+        unw_path, conncomp_path = dolphin.unwrap.unwrap(
+            ifg_filename=list_of_gtiff_ifgs[0],
+            corr_filename=corr_raster,
+            unw_filename=unw_filename,
+            nlooks=1,
+            init_method=init_method,
+        )
+        assert unw_path.exists()
+        assert conncomp_path.exists()
+
+    @pytest.mark.parametrize("method", [UnwrapMethod.ICU, UnwrapMethod.PHASS])
+    def test_unwrap_methods(self, tmp_path, raster_100_by_200, corr_raster, method):
+        unw_filename = tmp_path / f"{method.value}_unwrapped.unw.tif"
+        u_path, c_path = dolphin.unwrap.unwrap(
+            ifg_filename=raster_100_by_200,
+            corr_filename=corr_raster,
+            unw_filename=unw_filename,
+            nlooks=1,
+            unwrap_method=method,
+        )
+        assert u_path.exists()
+        assert c_path.exists()
+
+    def test_unwrap_logfile(self, tmp_path, raster_100_by_200, corr_raster):
+        unw_filename = tmp_path / "unwrapped.unw.tif"
+        u_path, c_path = dolphin.unwrap.unwrap(
+            ifg_filename=raster_100_by_200,
+            corr_filename=corr_raster,
+            unw_filename=unw_filename,
+            nlooks=1,
+            unwrap_method="icu",
+            log_to_file=True,
+        )
+        logfile_name = str(unw_filename).replace(".unw.tif", ".unw.log")
+        assert Path(logfile_name).exists()
+        assert u_path.exists()
+        assert c_path.exists()
+
+
+class TestUnwrapRun:
+    def test_run_gtiff(self, list_of_gtiff_ifgs, corr_raster):
+        ifg_path = list_of_gtiff_ifgs[0].parent
+        u_paths, c_paths = dolphin.unwrap.run(
+            ifg_filenames=list_of_gtiff_ifgs,
+            cor_filenames=[corr_raster] * len(list_of_gtiff_ifgs),
+            output_path=ifg_path,
+            nlooks=1,
+            init_method="mst",
+            max_jobs=1,
+        )
+        assert all(p.exists() for p in u_paths)
+        assert all(p.exists() for p in c_paths)
+
+    def test_run_envi(self, list_of_envi_ifgs, corr_raster):
+        ifg_path = list_of_envi_ifgs[0].parent
+        u_paths, c_paths = dolphin.unwrap.run(
+            ifg_filenames=list_of_envi_ifgs,
+            cor_filenames=[corr_raster] * len(list_of_envi_ifgs),
+            output_path=ifg_path,
+            nlooks=1,
+            init_method="mst",
+            max_jobs=1,
+        )
+        assert all(p.exists() for p in u_paths)
+        assert all(p.exists() for p in c_paths)
 
 
 @pytest.mark.skipif(
