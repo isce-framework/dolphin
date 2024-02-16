67430f218e2d9afa230344cdd4a7d7cf8633e514
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-02-16 13:55:08 -0500

    split out the isce3 unwrap to module
    leads to better parity with tophu/snaphu-py

--- src/dolphin/unwrap/_unwrap.py
+++ src/dolphin/unwrap/_unwrap.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from os import fspath
 from pathlib import Path
 from typing import Optional, Sequence, Union
 
-import numpy as np
 from tqdm.auto import tqdm
 
 from dolphin import io
@@ -15,9 +13,10 @@ from dolphin.utils import DummyProcessPoolExecutor, full_suffix
 from dolphin.workflows import UnwrapMethod
 
 from ._constants import CONNCOMP_SUFFIX, UNW_SUFFIX
+from ._isce3 import unwrap_isce3
 from ._snaphu_py import unwrap_snaphu_py
 from ._tophu import multiscale_unwrap
-from ._utils import _redirect_unwrapping_log, _zero_from_mask, create_combined_mask
+from ._utils import create_combined_mask
 
 logger = get_log(__name__)
 
@@ -253,30 +252,29 @@ def unwrap(
 
     if unwrap_method == UnwrapMethod.SNAPHU:
         # Pass everything to snaphu-py
-        return unwrap_snaphu_py(
+        unw_path, conncomp_path = unwrap_snaphu_py(
             ifg_filename,
             corr_filename,
             unw_filename,
             nlooks,
             ntiles=ntiles,
             tile_overlap=tile_overlap,
-            mask_file=mask_file,
+            mask_file=combined_mask_file,
             nproc=n_parallel_tiles,
             zero_where_masked=zero_where_masked,
             init_method=init_method,
             cost=cost,
             scratchdir=scratchdir,
         )
-
-    if any(t > 1 for t in ntiles):
-        return multiscale_unwrap(
+    elif any(t > 1 for t in ntiles):
+        unw_path, conncomp_path = multiscale_unwrap(
             ifg_filename,
             corr_filename,
             unw_filename,
             downsample_factor,
             ntiles=ntiles,
             nlooks=nlooks,
-            mask_file=mask_file,
+            mask_file=combined_mask_file,
             zero_where_masked=zero_where_masked,
             init_method=init_method,
             cost=cost,
@@ -284,84 +282,15 @@ def unwrap(
             scratchdir=scratchdir,
             log_to_file=log_to_file,
         )
-
-    # Now we're using PHASS or ICU within isce3
-    from isce3.io import Raster
-    from isce3.unwrap import ICU, Phass
-
-    shape = io.get_raster_xysize(ifg_filename)[::-1]
-    corr_shape = io.get_raster_xysize(corr_filename)[::-1]
-    if shape != corr_shape:
-        msg = f"correlation {corr_shape} and interferogram {shape} shapes don't match"
-        raise ValueError(msg)
-
-    ifg_raster = Raster(fspath(ifg_filename))
-    corr_raster = Raster(fspath(corr_filename))
-    UNW_SUFFIX = full_suffix(unw_filename)
-
-    # Get the driver based on the output file extension
-    if Path(unw_filename).suffix == ".tif":
-        driver = "GTiff"
-        opts = list(io.DEFAULT_TIFF_OPTIONS)
     else:
-        driver = "ENVI"
-        opts = list(io.DEFAULT_ENVI_OPTIONS)
-
-    # Create output rasters for unwrapped phase & connected component labels.
-    # Writing with `io.write_arr` because isce3 doesn't have creation options
-    io.write_arr(
-        arr=None,
-        output_name=unw_filename,
-        driver=driver,
-        like_filename=ifg_filename,
-        dtype=np.float32,
-        options=opts,
-    )
-    conncomp_filename = str(unw_filename).replace(UNW_SUFFIX, CONNCOMP_SUFFIX)
-    io.write_arr(
-        arr=None,
-        output_name=conncomp_filename,
-        driver="GTiff",
-        dtype=np.uint32,
-        like_filename=ifg_filename,
-        options=io.DEFAULT_TIFF_OPTIONS,
-    )
-
-    # The different raster classes have different APIs, so we need to
-    # create the raster objects differently.
-    unw_raster = Raster(fspath(unw_filename), True)
-    conncomp_raster = Raster(fspath(conncomp_filename), True)
-
-    _redirect_unwrapping_log(unw_filename, unwrap_method.value)
-
-    if zero_where_masked and mask_file is not None:
-        logger.info(f"Zeroing phase/corr of pixels masked in {mask_file}")
-        zeroed_ifg_file, zeroed_corr_file = _zero_from_mask(
-            ifg_filename, corr_filename, mask_file
+        unw_path, conncomp_path = unwrap_isce3(
+            ifg_filename,
+            corr_filename,
+            unw_filename,
+            mask_file=combined_mask_file,
+            unwrap_method=unwrap_method,
+            zero_where_masked=zero_where_masked,
         )
-        corr_raster = Raster(fspath(zeroed_corr_file))
-        ifg_raster = Raster(fspath(zeroed_ifg_file))
 
-    logger.info(
-        f"Unwrapping size {(ifg_raster.length, ifg_raster.width)} {ifg_filename} to"
-        f" {unw_filename} using {unwrap_method.value}"
-    )
-    if unwrap_method == UnwrapMethod.PHASS:
-        # TODO: expose the configuration for phass?
-        # If we ever find cases where changing help, then yes we should
-        # coherence_thresh: float = 0.2
-        # good_coherence: float = 0.7
-        # min_region_size: int = 200
-        unwrapper = Phass()
-    else:
-        unwrapper = ICU(buffer_lines=shape[0])
-
-    unwrapper.unwrap(
-        unw_raster,
-        conncomp_raster,
-        ifg_raster,
-        corr_raster,
-    )
-
-    del unw_raster, conncomp_raster
-    return Path(unw_filename), Path(conncomp_filename)
+    # TODO: post-processing steps go here:
+    return unw_path, conncomp_path
