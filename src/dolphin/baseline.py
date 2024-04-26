00535ce86113e18c070c7b2428ae5d50ef13c869
Ryan Burns <rtburns@jpl.nasa.gov>
2024-04-26 11:26:32 -0700

    Add perpendicular baseline function
    Note that since isce3 is not required at toplevel by any other modules,
    this module is not imported by default. It must be explicitly imported
    via e.g. `from dolphin import baseline`, pulling in the isce3 dependency.

---
+++ src/dolphin/baseline.py
@@ -0,0 +1,36 @@
+import isce3
+import numpy
+
+
+def perpendicular_baseline(
+    llh,
+    ell: isce3.core.Ellipsoid,
+    dop: isce3.core.LUT2d,
+    side: isce3.core.LookSide,
+    wvl: float,  # wavelength
+    ref_orbit: isce3.core.Orbit,
+    sec_orbit: isce3.core.Orbit,
+):
+    az, ref_rng = isce3.geometry.geo2rdr(llh, ell, ref_orbit, dop, wvl, side)
+    ref_pos, vel = ref_orbit.interpolate(az)
+
+    az, sec_rng = isce3.geometry.geo2rdr(llh, ell, sec_orbit, dop, wvl, side)
+    sec_pos, _ = sec_orbit.interpolate(az)
+
+    # Difference in position between the two passes
+    baseline = numpy.linalg.norm(sec_pos - ref_pos)
+
+    # Compute angle between LOS vector and baseline vector
+    # via the law of cosines
+    costheta = (ref_rng**2 + baseline**2 - sec_rng**2) / (2 * ref_rng * baseline)
+
+    sintheta = numpy.sqrt(1 - costheta**2)
+    perp = baseline * sintheta
+    par = baseline * costheta
+
+    targ_xyz = ell.lon_lat_to_xyz(llh)
+    direction = numpy.sign(
+        numpy.dot(numpy.cross(targ_xyz - ref_pos, sec_pos - ref_pos), vel)
+    )
+
+    return direction * perp
