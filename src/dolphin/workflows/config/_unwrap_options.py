03ae0314e120939ff234b852e7cf0e52b489a30e
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-06-05 14:33:40 -0400

    convert `dataclass` into Pydantic `BaseModel` for spurt

--- src/dolphin/workflows/config/_unwrap_options.py
+++ src/dolphin/workflows/config/_unwrap_options.py
@@ -9,7 +9,6 @@ from pydantic import (
     PrivateAttr,
     field_validator,
 )
-from pydantic.dataclasses import dataclass
 
 from dolphin._log import get_log
 
@@ -126,149 +125,83 @@ class TophuOptions(BaseModel, extra="forbid"):
     _to_tuple = field_validator("ntiles", "downsample_factor", mode="before")(to_tuple)
 
 
-@dataclass
-class SpurtGeneralSettings:
-    """Settings associated with breaking data into tiles.
-
-    Parameters
-    ----------
-    use_tiles: bool
-        Tile up data spatially.
-    output_folder: str
-        Path to output folder.
-
-    """
-
-    use_tiles: bool = True
-    intermediate_folder: str = "./emcf_tmp"
-    output_folder: str = "./emcf"
+class SpurtGeneralSettings(BaseModel):
+    use_tiles: bool = Field(default=True, description="Tile up data spatially.")
+    intermediate_folder: Path = Field(
+        default=Path("./emcf_tmp"), description="Path to intermediate folder."
+    )
+    output_folder: Path = Field(
+        default=Path("./emcf"), description="Path to output folder."
+    )
 
 
-@dataclass
-class SpurtTilerSettings:
-    """Class for holding tile generation settings.
+class SpurtTilerSettings(BaseModel):
+    """Class for holding tile generation settings."""
 
-    Parameters
-    ----------
-    max_tiles: int
-        Maximum number of tiles allowed.
-    target_points_for_generation: int
-        Number of points used for determining tiles based on density.
-    target_points_per_tile: int
-        Target points per tile when generating tiles.
-    dilation_factor: float
-        Dilation factor of non-overlapping tiles. 0.05 would lead to
-        10 percent dilation of the tile.
-
-    """
-
-    max_tiles: int = 16
-    target_points_for_generation: int = 120000
-    target_points_per_tile: int = 800000
-    dilation_factor: float = 0.05
-
-    def __post_init__(self):
-        if self.max_tiles < 1:
-            errmsg = f"max_tiles must be atleast 1, got {self.max_tiles}"
-            raise ValueError(errmsg)
-        if self.dilation_factor < 0.0:
-            errmsg = f"dilation_factor must be >= 0., got {self.dilation_factor}"
-            raise ValueError(errmsg)
-        if self.target_points_for_generation <= 0:
-            errmsg = (
-                "target_points_for_generation must be > 0,"
-                f" got {self.target_points_for_generation}"
-            )
-            raise ValueError(errmsg)
-        if self.target_points_per_tile <= 0.0:
-            errmsg = (
-                f"target_points_per_tile must be > 0, got {self.target_points_per_tile}"
-            )
-            raise ValueError(errmsg)
+    max_tiles: int = Field(16, description="Maximum number of tiles allowed.", ge=1)
+    target_points_for_generation: int = Field(
+        120000,
+        description="Number of points used for determining tiles based on density.",
+        gt=0,
+    )
+    target_points_per_tile: int = Field(
+        800000, description="Target points per tile when generating tiles.", gt=0
+    )
+    dilation_factor: float = Field(
+        0.05,
+        description=(
+            "Dilation factor of non-overlapping tiles. "
+            "0.05 would lead to 5 percent dilation of the tile."
+        ),
+        ge=0.0,
+    )
 
 
-@dataclass
-class SpurtSolverSettings:
-    """Settings associated with Extended Minimum Cost Flow (EMCF) workflow.
-
-    Parameters
-    ----------
-    worker_count: int
-        Number of workers for temporal unwrapping in parallel. Set value to <=0
-        to let workflow use default workers (ncpus - 1).
-    links_per_batch: int
-        Temporal unwrapping operations over spatial links are performed in batches
-        and each batch is solved in parallel.
-    t_cost_type: str
-        Temporal unwrapping costs. Can be one of 'constant', 'distance',
-        'centroid'.
-    t_cost_scale: float
-        Scale factor used in computing edge costs for temporal unwrapping.
-    s_cost_type: str
-        Spatial unwrapping costs. Can be one of 'constant', 'distance',
-        'centroid'.
-    s_cost_scale: float
-        Scale factor used in computing edge costs for spatial unwrapping.
-
-    """
-
-    worker_count: int = 0
-    links_per_batch: int = 10000
-    t_cost_type: str = "constant"
-    t_cost_scale: float = 100.0
-    s_cost_type: str = "constant"
-    s_cost_scale: float = 100.0
-
-    def __post_init__(self):
-        cost_types = {"constant", "distance", "centroid"}
-        if self.t_cost_type not in cost_types:
-            errmsg = f"t_cost_type must be one of {cost_types}, got {self.t_cost_type}"
-            raise ValueError(errmsg)
-        if self.s_cost_type not in cost_types:
-            errmsg = f"s_cost_type must be one of {cost_types}, got {self.s_cost_type}"
-            raise ValueError(errmsg)
-        if self.links_per_batch <= 0:
-            errmsg = f"links_per_batch must be > 0, got {self.links_per_batch}"
-            raise ValueError(errmsg)
-        if self.t_cost_scale <= 0.0:
-            errmsg = f"t_cost_scale must be > 0, got {self.t_cost_scale}"
-            raise ValueError(errmsg)
-        if self.s_cost_scale <= 0.0:
-            errmsg = f"s_cost_scale must be > 0, got {self.s_cost_scale}"
-            raise ValueError(errmsg)
+class SpurtSolverSettings(BaseModel):
+    worker_count: int = Field(
+        default=0,
+        description=(
+            "Number of workers for temporal unwrapping in parallel. "
+            "Set value to <=0 to let workflow use default workers (ncpus - 1)."
+        ),
+    )
+    links_per_batch: int = Field(
+        default=10000,
+        description=(
+            "Temporal unwrapping operations over spatial links are performed in"
+            " batches and each batch is solved in parallel."
+        ),
+        gt=0,
+    )
+    t_cost_type: Literal["constant", "distance", "centroid"] = Field(
+        default="constant", description="Temporal unwrapping costs."
+    )
+    t_cost_scale: float = Field(
+        default=100.0,
+        description="Scale factor used to compute edge costs for temporal unwrapping.",
+        gt=0.0,
+    )
+    s_cost_type: Literal["constant", "distance", "centroid"] = Field(
+        default="constant", description="Spatial unwrapping costs."
+    )
+    s_cost_scale: float = Field(
+        default=100.0,
+        description="Scale factor used to compute edge costs for spatial unwrapping.",
+        gt=0.0,
+    )
 
 
-@dataclass
-class SpurtMergerSettings:
-    """Class for holding tile merging settings.
-
-    Parameters
-    ----------
-    min_overlap_points: int
-        Minimum number of pixels in overlap region for it to be considered
-        valid.
-    method: str
-        Currently, only "dirichlet" is supported.
-    bulk_method: str
-        Method used to estimate bulk offset between tiles.
-
-    """
-
-    min_overlap_points: int = 25
-    method: str = "dirichlet"
-    bulk_method: str = "L2"
-
-    def __post_init__(self):
-        bulk_methods = {"integer", "L2"}
-        if self.bulk_method not in bulk_methods:
-            errmsg = (
-                f"bulk_method must be one of {bulk_methods}. got {self.bulk_method}"
-            )
-            raise ValueError(errmsg)
-
-        if self.method != "dirichlet":
-            errmsg = f"'dirichlet' is the only valid option, got {self.method}"
-            raise ValueError(errmsg)
+class SpurtMergerSettings(BaseModel):
+    min_overlap_points: int = Field(
+        default=25,
+        description="Minimum number of overlap pixels to be considered valid.",
+    )
+    method: Literal["dirichlet"] = Field(
+        default="dirichlet", description="Currently, only 'dirichlet' is supported."
+    )
+    bulk_method: Literal["integer", "L2"] = Field(
+        default="L2", description="Method used to estimate bulk offset between tiles."
+    )
 
 
 class SpurtOptions(BaseModel, extra="forbid"):
