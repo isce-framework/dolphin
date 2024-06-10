904dc6463ab334897695ea52fe3a3e7776b6ef5e
Ryan Burns <rtburns@jpl.nasa.gov>
2024-06-10 12:07:55 -0700

    Update baseline docstring, remove geo2rdr calls

--- src/dolphin/baseline.py
+++ src/dolphin/baseline.py
@@ -1,61 +1,55 @@
 import isce3
-import numpy
+import numpy as np
 
 
-def perpendicular_baseline(
-    llh,
+def compute(
+    llh: ArrayLike,
+    ref_pos: ArrayLike,
+    sec_pos: ArrayLike,
+    ref_rng: float,
+    sec_rng: float,
+    ref_vel: ArrayLike,
     ell: isce3.core.Ellipsoid,
-    dop: isce3.core.LUT2d,
-    side: isce3.core.LookSide,
-    wvl: float,  # wavelength
-    ref_orbit: isce3.core.Orbit,
-    sec_orbit: isce3.core.Orbit,
 ):
-    """Compute perpendicular baseline at a given geometric position.
+    """Compute the perpendicular baseline at a given geographic position.
 
     Parameters
     ----------
-    llh :
-        Lon/Lat/Height vector specifying target position
+    llh : ArrayLike
+        Lon/Lat/Height vector specifying the target position.
+    ref_pos : ArrayLike
+        Reference position vector (x, y, z) in ECEF coordinates.
+    sec_pos : ArrayLike
+        Secondary position vector (x, y, z) in ECEF coordinates.
+    ref_rng : float
+        Range from the reference satellite to the target.
+    sec_rng : float
+        Range from the secondary satellite to the target.
+    ref_vel : ArrayLike
+        Velocity vector (vx, vy, vz) of the reference satellite in ECEF coordinates.
     ell : isce3.core.Ellipsoid
-        Ellipsoid for the target surface
-    dop : isce3.core.LUT2d
-        Doppler LUT
-    side : isce3.core.LookSide
-        Look side
-    wvl : float
-        Wavelength
-    ref_orbit : isce3.core.Orbit
-        Reference orbit
-    sec_orbit : isce3.core.Orbit
-        Secondary orbit
+        Ellipsoid for the target surface.
 
     Returns
     -------
     float
-        Perpendicular baseline, in meters
+        Perpendicular baseline, in meters.
 
     """
-    az, ref_rng = isce3.geometry.geo2rdr(llh, ell, ref_orbit, dop, wvl, side)
-    ref_pos, vel = ref_orbit.interpolate(az)
-
-    az, sec_rng = isce3.geometry.geo2rdr(llh, ell, sec_orbit, dop, wvl, side)
-    sec_pos, _ = sec_orbit.interpolate(az)
-
     # Difference in position between the two passes
-    baseline = numpy.linalg.norm(sec_pos - ref_pos)
+    baseline = np.linalg.norm(sec_pos - ref_pos)
 
     # Compute angle between LOS vector and baseline vector
     # via the law of cosines
     costheta = (ref_rng**2 + baseline**2 - sec_rng**2) / (2 * ref_rng * baseline)
 
-    sintheta = numpy.sqrt(1 - costheta**2)
+    sintheta = np.sqrt(1 - costheta**2)
     perp = baseline * sintheta
     # par = baseline * costheta
 
     targ_xyz = ell.lon_lat_to_xyz(llh)
-    direction = numpy.sign(
-        numpy.dot(numpy.cross(targ_xyz - ref_pos, sec_pos - ref_pos), vel)
+    direction = np.sign(
+        np.dot(np.cross(targ_xyz - ref_pos, sec_pos - ref_pos), ref_vel)
     )
 
     return direction * perp
