89382b91e8e6db1d704af7da1be8d6f8fb28a622
seyeonjeon <133063223+seyeonjeon@users.noreply.github.com>
2024-06-11 17:16:11 -0700

    Add filtering module for unwrapped interferogram (#305)
    * Add filtering module for unwrapped interferogram
    
    * spelling error
    
    * spelling error
    
    * test_filtering.py added
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update src/dolphin/filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update tests/test_filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Update tests/test_filtering.py
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: seyeonjeon <133063223+seyeonjeon@users.noreply.github.com>
    
    * Update src/dolphin/filtering.py
    
    ---------
    
    Co-authored-by: Scott Staniewicz <scott.stanie@gmail.com>

---
+++ tests/test_filtering.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from dolphin import filtering
+
+
+def test_filter_long_wavelegnth():
+    # Check filtering with ramp phase
+    y, x = np.ogrid[-3:3:512j, -3:3:512j]
+    unw_ifg = np.pi * (x + y)
+    corr = np.ones(unw_ifg.shape, dtype=np.float32)
+
+    # Filtering
+    filtered_ifg = filtering.filter_long_wavelength(unw_ifg, corr, pixel_spacing=300)
+    np.testing.assert_allclose(
+        filtered_ifg[10:-10, 10:-10],
+        np.zeros(filtered_ifg[10:-10, 10:-10].shape),
+        atol=1.0,
+    )
