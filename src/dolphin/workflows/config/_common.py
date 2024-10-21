54404bc5cadeaafa478598d27c664f02475338b2
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2024-10-20 22:15:41 -0400

    default 0.001

--- src/dolphin/workflows/config/_common.py
+++ src/dolphin/workflows/config/_common.py
@@ -108,7 +108,7 @@ class PhaseLinkingOptions(BaseModel, extra="forbid"):
     )
     shp_method: ShpMethod = ShpMethod.GLRT
     shp_alpha: float = Field(
-        0.005,
+        0.001,
         description=(
             "Significance level (probability of false alarm) for SHP tests. Lower"
             " numbers include more pixels within the multilook window during covariance"
