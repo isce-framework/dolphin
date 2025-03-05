ac12423aac69fa8da9ba1f8354afd09b86c70deb
Scott Staniewicz <scott.stanie@gmail.com>
2025-03-04 22:24:11 -0500

    complete read test

--- tests/test_atmosphere_ionosphere.py
+++ tests/test_atmosphere_ionosphere.py
@@ -1,6 +1,4 @@
-"""
-Test IONEX functionalities: file reading and interpolation
-"""
+"""Test IONEX functionalities: file reading and interpolation."""
 
 from pathlib import Path
 
@@ -31,6 +29,38 @@ def test_read_ionex():
     # Read IONEX tec_maps data - ignore mins, lats, and lons
     times, lats, lons, tec_maps = ionosphere.read_ionex(TEST_TEC_FILE)[:4]
     assert np.allclose(tec_maps[time_ind, y0:y1, x0:x1], tec_aoi)
+    # Check the shape of the data
+
+    assert times.shape == (13,)
+    assert lats.shape == (71,)
+    assert lons.shape == (73,)
+    assert tec_maps.shape == (13, 71, 73)
+    #     (array([   0.,  120.,  240.,  360.,  480.,  600.,  720.,  840.,  960.,
+    #         1080., 1200., 1320., 1440.]),
+    #  array([ 87.5,  85. ,  82.5,  80. ,  77.5,  75. ,  72.5,  70. ,  67.5,
+    #          65. ,  62.5,  60. ,  57.5,  55. ,  52.5,  50. ,  47.5,  45. ,
+    #          42.5,  40. ,  37.5,  35. ,  32.5,  30. ,  27.5,  25. ,  22.5,
+    #          20. ,  17.5,  15. ,  12.5,  10. ,   7.5,   5. ,   2.5,   0. ,
+    #          -2.5,  -5. ,  -7.5, -10. , -12.5, -15. , -17.5, -20. , -22.5,
+    #         -25. , -27.5, -30. , -32.5, -35. , -37.5, -40. , -42.5, -45. ,
+    #         -47.5, -50. , -52.5, -55. , -57.5, -60. , -62.5, -65. , -67.5,
+    #         -70. , -72.5, -75. , -77.5, -80. , -82.5, -85. , -87.5]),
+    #  array([-180., -175., -170., -165., -160., -155., -150., -145., -140.,
+    #         -135., -130., -125., -120., -115., -110., -105., -100.,  -95.,
+    #          -90.,  -85.,  -80.,  -75.,  -70.,  -65.,  -60.,  -55.,  -50.,
+    #          -45.,  -40.,  -35.,  -30.,  -25.,  -20.,  -15.,  -10.,   -5.,
+    #            0.,    5.,   10.,   15.,   20.,   25.,   30.,   35.,   40.,
+    #           45.,   50.,   55.,   60.,   65.,   70.,   75.,   80.,   85.,
+    #           90.,   95.,  100.,  105.,  110.,  115.,  120.,  125.,  130.,
+    #          135.,  140.,  145.,  150.,  155.,  160.,  165.,  170.,  175.,
+    #          180.]),
+
+    assert np.allclose(times[:4], [0, 120, 240, 360])
+    assert np.allclose(times[-4:], [1080, 1200, 1320, 1440])
+
+    assert np.allclose(lats[:4], [87.5, 85, 82.5, 80])
+
+    assert np.allclose(lons[:4], [-180, -175, -170, -165])
 
 
 def test_get_ionex_value():
